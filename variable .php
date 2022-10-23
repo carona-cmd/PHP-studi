53b24952bd6e71e54e7b9eeb1e6783e93708e801
carona-md <alissars2001@yahoo.com>
2022-10-23 06:04:26 +0200

    numbercommit

---
+++ variable .php
@@ -0,0 +1,78 @@
+<?php
+/*/ /
+
+un saut de ligne
+//stocker des valeurs pour résoudre un problème =>traiter par un ordinateur
+//variable//
+
+/
+$premier = "hello"
+$deuxième
+//variable => au cours de l execution du code modifier la valeur stockée dans les variables 
+$premier = 2 
+$premier = "retour"
+//constantes 
+creer   ce type de variable et une fois que l on va donner une valeur a cette constante = impossible
+define()
+*/
+
+define ("URL", https://yahoo.fr");
+var_dump(URL); //affiche le contenu de CONSTANTE URL 
+$variable = "toto";
+//constante 
+define("ROOT,"c://)");
+
+//NON souvent => librairie
+//creer un fchier config.php
+define ("NOM_BD" ,"demo");
+define ("NOM_BD" ,"demo");
+define ("NOM_BD" ,"demo");
+// creer une varaible
+$adresse =5 rue de ferreire venedeville ;
+function generateFicheClient(){}
+var_dump();// EVITER DE REECRIRE LE CODE 
+
+
+define ("Ville","Paris");
+function genererVille(){
+    var_dump($ville);
+    tableau indexé => les avleurs dans le tableau sont utilisables selon la position qui leur est donnée 
+    var_dump($paramètre[0]);
+}
+
+// tableaux associatifs 
+$data =[
+
+    "largeur"=> 200, 
+    "unité"=> "px",
+    "couleur-fond"=>"red"
+];
+$date2 = array (
+    "police"=> "arial";
+    "font-weight "=> 500,
+    "border "=> '2 px solid green'
+);
+// objet 
+// php il faut d abrd creer une classe 
+
+class Etudiant{
+    public $nom ="alain";
+    public $age =22;
+    public $cometences =["PHP", "html", "java"];
+
+}
+$Etudiant = new Etudiant ();
+//$etudiant instance de classe = objet
+var_dump($etudiant->nom);
+var_dump($etudiant);
+// resoudre un pb en informatique
+// variables stocker des valeurs
+// string / number / booelen / 
+// https :// github.com /webdevprformation/php-studi
+// can i use : site pour html css et javacsript
+// php W3school: tous les concepts de php 
+//EOL : constante de php 
+var_dump("bonjour".EOL)// saut de ligne
+print_r("bonjour",PHP_EOL."commentaller vous??");
+print_r()
+$width = 300;
