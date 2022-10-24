53089534ab94817b53d8931d00db61b8631b328d
carona-md <alissars2001@yahoo.com>
2022-10-24 07:35:04 +0200

    commit1

---
+++ boucles.php
@@ -0,0 +1,41 @@
+<?php 
+ // loop = boucle
+// il est possible de faire repeter des isntructions sans avoir a les ecrire plusieurs fois 
+// ecrire 1 fois une instruction => php va execuetr cete instructuon plusisurs fois sans avoir a la reecrire
+// for()
+// while 
+// cas particuleir pour les tableaux quand on vet parcurir un tableau 
+// foreach
+// for: dans les parentheses de for on a 3 instructions: 
+    // $i =0 on cree une variable et on l initialise a 0 (i pour iterateur)
+    // 2 eme instruction$i <3(boleene conditiond esortie de la boucle )
+    // l increment $i++ pas de point virgule a la fin, on augmente la valeur d e$i de 1 te on restocke les valeurs dans i
+    // tant que la condition est true, executer les isntructions dans les accolades de la bouclefor 
+    for( $i = 0 ; $i<3; $i = $i ++ ) {
+                  // condition de sortie de al boucle 
+                  //      $i = $i + 1 
+                  //      $i = 0 + 1 
+                  //      $i = 1
+            // $i<3  => true
+                  //      $i = $i + 1 
+                  //      $i = 1 + 1
+                  //      $i = 2 
+            // 2 < 3 => true
+            //      $i = $i + 1 
+            //      $i = 2 + 1
+            //      $i = 3
+            
+            // 3  <3 = > false : j arret d executer ; tant que la condition de sortie est vraie on l execute
+            
+
+        echo "$i<br>";
+
+    //}
+    // si on veut executer 2 fois ce code , on ferait une boucle imbriquée ? 
+    for ($i = 0 ; $i<2 ;  $i++) {
+        for($j = 0 ; $j< 3 ; $j++){
+            echo $j."<br>";
+        }
+    }
+
+    
---
+++ while.php
@@ -0,0 +1,18 @@
+<?PHP 
+
+// while : 3 instructions, creer une variable, creer une condition de sortie puis l increment
+$j = 2;
+while ($j < 12){
+    var_dump($j);
+    $j+= 2; 
+}
+// 2 4 6 8 
+// on peut augmenter avnat de faire les instructions, 
+// condition de sortie tres important sinon ce sera des boucles infinies 
+// ex : boucle infinie 
+/*$k = 13 ;
+while($k >12){
+    var_dump($k);
+    $k++; 
+
+}*/
